f7252eaa54857b2961c9b6a53eb256c71f4e2b7c
gauenk <kent.gauen@gmail.com>
2023-04-08 17:49:56 -0400

    dev train colanet

--- scripts/trte_colanet/train.py
+++ scripts/trte_colanet/train.py
@@ -25,8 +25,7 @@ def main():
     print("PID: ",pid)
 
     # -- records --
-    def clear_fxn(num,cfg):
-        return False
+    def clear_fxn(num,cfg): return False
     exps,uuids = cache_io.train_stages.run("exps/trte_colanet/train.cfg",
                                            ".cache_io/trte_colanet/train/")
     # -- filter --
