42e8a79149010c06e2602bb90c9028fd84a6be54
gauenk <kent.gauen@gmail.com>
2023-05-08 12:13:53 -0400

    new scheduler

--- dev/viz_scheduler.py
+++ dev/viz_scheduler.py
@@ -3,6 +3,7 @@ import torch as th
 import torch.nn as nn
 from pathlib import Path
 from matplotlib import pyplot as plt
+from linear_warmup_cosine_annealing_warm_restarts_weight_decay import ChainedScheduler
 
 class NullModule(nn.Module):
     def __init__(self):
@@ -23,33 +24,45 @@ def main():
     model = NullModule()
     lr = 5e-6
     optimizer = th.optim.Adam(model.parameters(),lr=lr,weight_decay=1e-6)
-    ChainedScheduler = th.optim.lr_scheduler.ChainedScheduler
+    # ChainedScheduler = th.optim.lr_scheduler.ChainedScheduler
+    SequentialLR = th.optim.lr_scheduler.SequentialLR
     ExponentialLR = th.optim.lr_scheduler.ExponentialLR
     CosineAnnealingWarmRestarts = th.optim.lr_scheduler.CosineAnnealingWarmRestarts
 
     T_0 = 200
     T_mult = 2
     eta_min = 1e-8
-    gamma = 0.99
+    gamma = 0.9
     print(optimizer)
 
     # scheduler0 = ExponentialLR(optimizer, gamma=gamma)
-    scheduler1 = CosineAnnealingWarmRestarts(optimizer, T_0,
-                                             T_mult=T_mult,eta_min=eta_min)
-    # scheduler = ChainedScheduler([scheduler1,scheduler0])
-    scheduler = scheduler1
+    # scheduler1 = CosineAnnealingWarmRestarts(optimizer, T_0,
+    #                                          T_mult=T_mult,eta_min=eta_min)
+    # scheduler = SequentialLR(optimizer,[scheduler0,scheduler1],milestones=[100])
+    # scheduler = ChainedScheduler([scheduler1,scheduler0],warmup_steps=10)
+    # scheduler = scheduler1
+
+    scheduler = ChainedScheduler(
+        optimizer,
+        T_0 = T_0,
+        T_mul = T_mult,
+        eta_min = eta_min,
+        gamma = gamma,
+        max_lr = 1.0,
+        warmup_steps= 100)
+
     print(scheduler)
 
     print(dir(scheduler))
     # print(scheduler.T_0,scheduler.T_cur,scheduler.T_i,
     #       scheduler.T_mult,scheduler._step_count,scheduler.eta_min,
-    #       scheduler.get_last_lr())
-    print(scheduler.get_last_lr())
+    #       scheduler.get_lr())
+    # print(scheduler.get_lr())
     scheduler.step()
     # print(scheduler.T_0,scheduler.T_cur,scheduler.T_i,
     #       scheduler.T_mult,scheduler._step_count,scheduler.eta_min,
-    #       scheduler.get_last_lr())
-    print(scheduler.get_last_lr())
+    #       scheduler.get_lr())
+    print(scheduler.get_lr())
     nsteps = 1000
     lrs = collect_lrs(optimizer,scheduler,nsteps)
     print(len(lrs))
