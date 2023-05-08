b844aaf4dc65c2708c68c4e07a38f3b1230ab9a4
Kent W Gauen <gauenk@gilbreth-fe00.rcac.purdue.edu>
2023-05-08 16:12:17 -0400

    update base f2f

--- scripts/trte_f2f/train.py
+++ scripts/trte_f2f/train.py
@@ -32,8 +32,8 @@ def main():
 
     # -- get/run experiments --
     exps,uuids = cache_io.train_stages.run("exps/trte_f2f/train.cfg",
-                                           ".cache_io_exps/trte_f2f/train/",
-                                           update=True)
+                                           ".cache_io_exps/trte_f2f/train/",update=True)
+
     print(exps[0])
     print(len(exps))
     def clear_fxn(num,cfg): return False
