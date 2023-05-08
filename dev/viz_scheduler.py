1560e54cc7b1d7f6abc62295b3759e41817f40cb
gauenk <kent.gauen@gmail.com>
2023-05-08 19:11:38 -0400

    update f2f

--- dev/viz_scheduler.py
+++ dev/viz_scheduler.py
@@ -26,9 +26,11 @@ def main():
     lr = 3e-4
     optimizer = th.optim.Adam(model.parameters(),lr=lr,weight_decay=1e-6)
     # ChainedScheduler = th.optim.lr_scheduler.ChainedScheduler
+    StepLR = th.optim.lr_scheduler.StepLR
+    MultiStepLR = th.optim.lr_scheduler.MultiStepLR
     SequentialLR = th.optim.lr_scheduler.SequentialLR
-    CosineAnnealingLR = th.optim.lr_scheduler.CosineAnnealingLR
     ExponentialLR = th.optim.lr_scheduler.ExponentialLR
+    CosineAnnealingLR = th.optim.lr_scheduler.CosineAnnealingLR
     CosineAnnealingWarmRestarts = th.optim.lr_scheduler.CosineAnnealingWarmRestarts
 
     T_0 = 200
@@ -38,6 +40,8 @@ def main():
     gamma = 0.9
     nsteps = 100
     print(optimizer)
+    gamma = math.exp(math.log(lr_final/lr)/nsteps)
+    print(gamma)
 
     # scheduler0 = ExponentialLR(optimizer, gamma=gamma)
     # scheduler1 = CosineAnnealingWarmRestarts(optimizer, T_0,
@@ -54,7 +58,14 @@ def main():
     #     gamma = gamma,
     #     max_lr = 1.0,
     #     warmup_steps= 100)
-    scheduler = CosineAnnealingLR(optimizer,100)
+    # scheduler = CosineAnnealingLR(optimizer,100)
+    # scheduler = ExponentialLR(optimizer, gamma=gamma)
+    step_size = 5
+    gamma = math.exp(math.log(lr_final/lr)/(nsteps//step_size))
+    # scheduler = StepLR(optimizer,step_size=step_size,gamma=gamma)
+    milestones = 10*np.arange(10)[2::2]
+    print(milestones)
+    scheduler = MultiStepLR(optimizer,milestones=milestones,gamma=.5)
     # gamma = math.exp(math.log(lr_final/lr)/nsteps)
     # print(gamma)
     # scheduler = ExponentialLR(optimizer,gamma=gamma)
@@ -74,6 +85,7 @@ def main():
     lrs = collect_lrs(optimizer,scheduler,nsteps)
     print(len(lrs))
     plt.plot(lrs)
+    print(lrs[-1])
     root = Path("output/viz_schedulers/")
     if not(root.exists()): root.mkdir()
     plt.savefig(root / "lrs.png")
--- scripts/trte_f2f/train.py
+++ scripts/trte_f2f/train.py
@@ -32,7 +32,6 @@ def main():
 
     # -- get/run experiments --
     exps,uuids = cache_io.train_stages.run("exps/trte_f2f/train.cfg",
-                                           ".cache_io_exps/trte_f2f/train/")
                                            ".cache_io_exps/trte_f2f/train/",update=True)
 
     print(exps[0])
