ec1fbc157adb7c2554350925f8971151c49c92e5
Kent W Gauen <gauenk@gilbreth-fe00.rcac.purdue.edu>
2023-05-09 09:19:56 -0400

    [dev] f2f test

--- scripts/trte_f2f/test.py
+++ scripts/trte_f2f/test.py
@@ -37,13 +37,13 @@ def main():
     print("Run Exps: ",len(exps))
 
     # -- run exps --
-    results = cache_io.run_exps(exps,test.run,uuids=uuids,
+    results = cache_io.run_exps(exps,test.run,uuids=uuids,preset_uuids=True,
                                 name=".cache_io/trte_f2f/test",
                                 version="v1",skip_loop=False,
                                 clear=False,enable_dispatch="slurm",
                                 clear_fxn=clear_fxn,
                                 records_fn=".cache_io_pkl/trte_f2f/test.pkl",
-                                records_reload=False,to_records_fast=False,
+                                records_reload=False,to_records_fast=True,
                                 use_wandb=True,proj_name="neurips_test_f2f")
 
     # -- misc --
@@ -64,7 +64,7 @@ def main():
     # gfields = ["dist_crit","k","ws","ps","nepochs","ps_dists","stride0",
     #            "iphone_type"]
     # gfields = ["crit_name","nepochs","iphone_type"]
-    gfields = ["crit_name","nepochs","dname"]
+    gfields = ["crit_name","nepochs","dname",'sigma','rate',"tr_uuid"]
     # optional = ["iphone_type","stnls_center_crop","label0"]
     optional = ["stnls_center_crop","label0"]
     for opt in optional:
@@ -86,7 +86,8 @@ def main():
     # -- split groups --
     key = ['psnrs','ssims','strred','psnrs_pp','ssims_pp','strred_pp']
     # key = 'ssims'
-    gfields = ["crit_name","nepochs",]
+    # gfields = ["crit_name","nepochs",]
+    gfields = ["crit_name","nepochs","dname",'sigma','rate',"tr_uuid"]
     optional = ["stnls_center_crop"]
     # optional = ["iphone_type","stnls_center_crop"]
     for opt in optional:
@@ -94,7 +95,7 @@ def main():
             gfields += [opt,]
 
     # gfields = ["crit_name","nepochs"]
-    for group0,gdf0 in summary.groupby("label0"):
+    for group0,gdf0 in summary.groupby(["tr_uuid","dname","sigma","rate"]):
         print(group0)
         for group,gdf in gdf0.groupby(gfields):
             print(gdf[key].to_numpy()," : ",group)
