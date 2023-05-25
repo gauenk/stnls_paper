6cc4514d3d5b6486cd9fe313b213cfc8acb99efe
gauenk <kent.gauen@gmail.com>
2023-05-24 23:22:10 -0400

    nlnet design looks good.

--- scripts/trte_nlnet/train.py
+++ scripts/trte_nlnet/train.py
@@ -32,13 +32,35 @@ def main():
     #                "save_epoch_list":"1-50-100-150-200-250"}
     # read_filter = None
     exps,uuids = cache_io.train_stages.run("exps/trte_nlnet/train.cfg",
-                                           ".cache_io_exps/trte_nlnet/train/")#,update=True)
+                                           ".cache_io_exps/trte_nlnet/train/",update=True)
     print(uuids)
 
     # -- view --
-    # bench.print_summary(exps,(1,3,3,128,128))
-    # print(len(exps))
-    # exit(0)
+    # bench.print_summary(exps[0],(1,3,3,128,128))
+    # bench.print_summary(exps[0],(1,3,3,512,512))
+    results = []
+    # vshape = (1,10,3,512,512)
+    # vshape = (1,5,3,360,360)
+    # vshape = (2,6,3,256,256)
+    vshape = (4,4,3,256,256)
+    # vshape = (1,10,3,256,256)
+    # vshape = (1,10,3,280,280)
+    n = 0
+    for exp in exps:
+        res = bench.summary(exp,vshape,with_flows=True)
+        res.arch_depth = exp.arch_depth
+        res.arch_nheads = exp.arch_nheads
+        res.embed_dim = exp.embed_dim
+        results.append(res)
+        # if n > 5: break
+        n+=1
+    results = pd.DataFrame(results)
+    print(results[['arch_depth','arch_nheads','embed_dim']])
+    print(results[['timer_fwd','trainable_params',"macs"]])
+    print(results[['timer_fwd','timer_bwd']])
+    print(results[['alloc_fwd','alloc_bwd','res_fwd','res_bwd']])
+    print(results[['fwdbwd_mem','trainable_params','macs']])
+    exit(0)
     # -- get bench--
     # for i in range(32):
     #     print(i,exps[i])
